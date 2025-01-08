b51d2ef1c84a0d98dd6ab3d4ed965316a82d2fe1
manjunathbhavi04 <manjunathbhavi890@gmail.com>
2025-01-08 09:14:46 +0530

    added repo layer to student class

---
+++ src/main/java/com/example/student_management_app/StudentRepository.java
@@ -0,0 +1,4 @@
+package com.example.student_management_app;
+
+public class StudentRepository {
+}
--- src/main/java/com/example/student_management_app/StudentService.java
+++ src/main/java/com/example/student_management_app/StudentService.java
@@ -1,4 +1,9 @@
 package com.example.student_management_app;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentService {
+        
 }
